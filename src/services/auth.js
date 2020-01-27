a921d3c6f947c7d83f086ac099a63746eadf3391
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2020-01-27 11:10:33 -0500

    [SERVICE]: Auth added and routes

---
+++ src/services/auth.js
@@ -0,0 +1,86 @@
+import { Service, Inject } from 'typedi';
+import jwt from 'jsonwebtoken';
+import argon2 from 'argon2';
+import { randomBytes } from 'crypto';
+import config from '../config';
+
+export default class AuthService {
+    constructor(container) {
+        this.userModel = container.get('userModel');
+        this.logger = container.get('logger');
+    }
+
+    async SignUp({ name, email, password }) {
+        try {
+            const salt = randomBytes(32);
+
+            this.logger.silly('Hashing password');
+            const hashedPassword = await argon2.hash(password, { salt });
+
+            this.logger.silly('Creating user DB record');
+            const userRecord = await this.userModel.create({
+                name,
+                email,
+                salt: salt.toString('hex'),
+                password: hashedPassword
+            });
+
+            this.logger.silly('Generating JWT');
+            const token = this.generateToken(userRecord);
+            
+            const user = userRecord.toObject();
+            Reflect.deleteProperty(user, 'password');
+            Reflect.deleteProperty(user, 'salt');
+
+            return { user, token };
+        } catch (e) {
+            this.logger.error(e);
+
+            throw e;
+        }
+    }
+
+    async SignIn(email, password) {
+        const userRecord = await this.userModel.findOne({ email });
+
+        if (!userRecord) {
+            throw new Error('User not registered');
+        }
+
+        this.logger.silly('Checking password');
+        const validPassword = await argon2.verify(userRecord.password, password);
+
+        if (!validPassword) {
+            throw new Error('Invalid Password');
+        }
+
+        this.logger.silly('Password is valid!');
+        this.logger.silly('Generating JWT');
+        const token = this.generateToken(userRecord);
+
+        const user = userRecord.toObject();
+        Reflect.deleteProperty(user, 'password');
+        Reflect.deleteProperty(user, 'salt');
+
+        return { user, token };
+    }
+
+    generateToken(user) {
+        const today = new Date();
+        const exp = new Date(today);
+
+        exp.setDate(today.getDate() + 60);
+
+        this.logger.silly(`Sign JWT for userId: ${user._id}`);
+
+        return jwt.sign(
+            {
+                _id: user._id,
+                role: user.role,
+                name: user.name,
+                exp: exp.getTime() / 1000,
+            }, 
+            config.jwtSecret
+        );
+    }
+}
